285c786e05d7f859b8011cb1d5c51f677e5265a1
ushaph <ushaph598@gmail.com>
2025-06-03 10:23:49 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,30 @@
+
+
+//calculator program
+const display = document.getElementById("display");
+
+function appendtodisplay(input){
+    display.value += input;
+}
+
+
+function clearDisplay(){
+    display.value ="";
+}
+
+
+
+function calculate(){
+
+    try{
+        display.value =eval(display.value);
+
+    }
+
+    catch(error){
+        display.value ="error";
+
+    }  
+}
+
+
